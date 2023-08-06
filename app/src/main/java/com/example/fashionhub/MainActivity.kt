76c4cd652c22037e7327794f852531e218dc9069
Ali Hamza <officer3765@gmail.com>
2023-08-06 13:17:04 +0500

    Signup activity updated,Signup activity design is under working

--- app/src/main/java/com/example/fashionhub/MainActivity.kt
+++ app/src/main/java/com/example/fashionhub/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.fashionhub
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textview.MaterialTextView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var btn: Button;
+    lateinit var signup: MaterialTextView;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        signup=findViewById(R.id.tvsignup);
+        signup.setOnClickListener {
+            val intent=Intent(this,ActivitySignUp::class.java);
+            startActivity(intent);
+        }
     }
 }
