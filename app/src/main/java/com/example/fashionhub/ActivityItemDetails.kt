b0560f146f89dc2f018f47cd2edbaec7edbfa1f8
ranaMuzammil89 <muzammilahamd893@gmail.com>
2023-08-19 17:59:26 +0500

    addToCart activity created and edited

--- app/src/main/java/com/example/fashionhub/ActivityItemDetails.kt
+++ app/src/main/java/com/example/fashionhub/ActivityItemDetails.kt
@@ -2,17 +2,49 @@ package com.example.fashionhub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import com.example.fashionhub.databinding.ActivityItemDetailsBinding
 
 class ActivityItemDetails : AppCompatActivity() {
 
 
-    private var binding: ActivityItemDetailsBinding? = null
+    private lateinit var binding: ActivityItemDetailsBinding
+    private var itemCount=0
+    private var selectedCard: CardView?=null
+    private var selectedText: TextView?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityItemDetailsBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+
+        binding.backarrow.setOnClickListener {
+            super.onBackPressed()
+        }
+
+        binding.decreaseButton.setOnClickListener {
+            if (itemCount>0){
+                itemCount--
+                updateCounter()
+            }
+        }
+        binding.increaseButton.setOnClickListener {
+            itemCount++
+            updateCounter()
+        }
+
+        binding.small.setOnClickListener {
+            onCardClicked(binding.small, binding.s)
+        }
+
+        binding.medium.setOnClickListener {
+            onCardClicked(binding.medium, binding.m)
+        }
+
+        binding.large.setOnClickListener {
+            onCardClicked(binding.large, binding.l)
+        }
 
         // Retrieve the serialized item data from the intent
         val selectedItem = intent.getParcelableExtra<Items>("selectedItem")
@@ -26,9 +58,27 @@ class ActivityItemDetails : AppCompatActivity() {
         }
     }
 
+    private fun onCardClicked(clickedCard: CardView, clickedTextView: TextView) {
+
+
+        selectedCard?.setCardBackgroundColor(resources.getColor(android.R.color.white))
+        selectedText?.setTextColor(resources.getColor(android.R.color.black))
+
+        selectedCard = clickedCard
+        selectedCard?.setCardBackgroundColor(resources.getColor(R.color.black))
+
+        selectedText = clickedTextView
+        selectedText?.setTextColor(resources.getColor(android.R.color.white))
+
+        // Perform other actions for the selected product
+    }
+
+    private fun updateCounter() {
+        binding.countTextView.text=itemCount.toString()
+    }
+
     override fun onDestroy() {
         super.onDestroy()
-        binding = null
     }
 
 }
---
+++ app/src/main/java/com/example/fashionhub/Ui/ActivityCart.kt
@@ -0,0 +1,12 @@
+package com.example.fashionhub.Ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fashionhub.R
+
+class ActivityCart : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+    }
+}
