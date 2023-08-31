4652b04e377d75b3355b8c3ea154a0ea33cd6ef2
ranaMuzammil89 <muzammilahamd893@gmail.com>
2023-08-31 11:27:41 +0500

    activity cart modified

--- app/src/main/java/com/example/fashionHub/ui/ActivityCart.kt
+++ app/src/main/java/com/example/fashionHub/ui/ActivityCart.kt
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.fashionHub.model.ItemToCartModel
 import com.example.fashionhub.R
+import com.example.fashionhub.databinding.ActivityCartBinding
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -20,9 +21,12 @@ class ActivityCart : AppCompatActivity() {
     private lateinit var adaptor: AdaptorCartItems
     private lateinit var userArrayList: ArrayList<ItemToCartModel>
     private lateinit var recyclerView: RecyclerView
+    private lateinit var binding: ActivityCartBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cart)
+        binding=ActivityCartBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
 
 
 
@@ -58,21 +62,23 @@ class ActivityCart : AppCompatActivity() {
         })
         // Calculate and display initial total price
         updateTotalPrice()
-        updateSelectedItemsTax()
     }
 
     fun updateTotalPrice() {
-        val totalPrice = calculateTotalPrice()
+        val (price,tax)=calculateTotalPrice()
         // Update your UI element (e.g., a TextView) to display the total price
         // For example, if you have a TextView with the ID "totalPriceTextView":
         val totalPriceTextView: TextView = findViewById(R.id.subTotalPrice)
         // Convert the total price back to a string before setting it in the TextView
-        totalPriceTextView.text = "$${totalPrice.toString()}"
+        totalPriceTextView.text = "$${price.toString()}"
+        binding.gstPrice.text=tax.toString()
     }
 
-    private fun calculateTotalPrice(): Double {
+    private fun calculateTotalPrice(): Pair<Double, Int> {
 
         var total = 0.0
+        var totalQuantity=0
+        var totalQuantityTax=0.0
         for (item in userArrayList) {
             if (item.isSelected) {
                 val itemPriceString = "${item.price}".replace("$", "")
@@ -80,26 +86,13 @@ class ActivityCart : AppCompatActivity() {
                 // Convert the price string to double for calculation
                 val itemPrice = itemPriceString.toDouble()
                 total += itemPrice * item.quantity.toInt()
-            }
-        }
-        return total
-    }
-    private fun calculateSelectedItemsQuantity(): Int {
-        var totalQuantity = 0
-
-        for (item in userArrayList) {
-            if (item.isSelected) {
                 totalQuantity += item.quantity.toInt()
-                totalQuantity*=totalQuantity
             }
         }
+        // Calculate the total quantity with 15% tax
+        totalQuantityTax = totalQuantity * 1.15
 
-        return totalQuantity
-    }
-    fun updateSelectedItemsTax() {
-        val selectedItemsQuantity = calculateSelectedItemsQuantity()
-        val selectedItemsQuantityTextView: TextView = findViewById(R.id.gstPrice)
-        selectedItemsQuantityTextView.text = selectedItemsQuantity.toString()
+        return Pair(total,totalQuantityTax.toInt())
     }
 
 }
